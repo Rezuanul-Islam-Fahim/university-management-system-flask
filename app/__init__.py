05076a1ee19a84d6595194b3634b9260cbb9784d
Rezuanul Islam Fahim <rifahim98@gmail.com>
2024-01-01 13:15:41 +0600

    Initialize flask_sqlalchemy with app

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 
 from config import Config
+from app.db import db
 
 
 def create_app(config=Config):
 
     app = Flask(__name__)
     app.config.from_object(config)
+
+    db.init_app(app)
 
     return app
