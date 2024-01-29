d8707b224a8fe5c3d1560dc82e79136001bbfb42
Rezuanul Islam Fahim <rifahim98@gmail.com>
2024-01-29 17:19:29 +0600

    Initialize migration and create user table schema

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_migrate import Migrate
 
 from config import Config
 from app.db import db
@@ -10,6 +11,7 @@ def create_app(config=Config):
     app.config.from_object(config)
 
     db.init_app(app)
+    Migrate(app, db)
 
     from app.web import AuthBlueprint
     app.register_blueprint(AuthBlueprint, url_prefix='/auth')
--- app/models/user.py
+++ app/models/user.py
@@ -9,18 +9,20 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), unique=True, nullable=False)
-    full_name = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, unique=True, nullable=False)
+    full_name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(180), unique=True)
     password_hash = db.Column(db.String, nullable=False)
+    profile_img = db.Column(db.String)
 
-    def __init__(self, username, full_name, email, password):
+    def __init__(self, username, full_name, email, password, profile_img):
 
         self.username = username
         self.full_name = full_name
         self.email = email
         self.password_hash = generate_password_hash(password)
+        self.profile_img = profile_img
 
     def __repr__(self):
 
         return f'Username: {self.username}, Full Name: {self.full_name}, \
-                Email: {self.email}'
+                Email: {self.email}, Profile image: {self.profile_img}'
